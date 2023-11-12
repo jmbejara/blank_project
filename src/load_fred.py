999f93b4d83fb85efcb1431e9bf1ea93d37cfb96
Jeremy Bejarano <jmbejara@gmail.com>
2023-11-11 22:52:48 -0500

    Run pytest unit tests with GitHub actions

--- src/load_fred.py
+++ src/load_fred.py
@@ -6,26 +6,37 @@ from pathlib import Path
 DATA_DIR = Path(config.data_dir)
 
 
-def pull_and_save_fred_data(start="1913-01-01", end="2023-10-01", data_dir=DATA_DIR):
-    # Load CPI data from FRED, seasonally adjusted
-    df = pandas_datareader.get_data_fred(["CPIAUCNS", "GDP", "GDPC1"], start=start, end=end)
-    file_dir = Path(data_dir) / "pulled"
-    file_dir.mkdir(parents=True, exist_ok=True)
-    df.to_csv(file_dir / "fred_cpi.csv")
-    # df.to_parquet(file_dir / 'fred_cpi.parquet')
-
-
-def load_fred(data_dir=DATA_DIR):
+def load_fred(
+    data_dir=DATA_DIR,
+    from_cache=True,
+    save_cache=False,
+    start="1913-01-01",
+    end="2023-10-01",
+):
     """
-    Must first run pull_and_save_fred_data
+    Must first run pull_and_save_fred_data. If loading from cache, then start
+    and dates are ignored
     """
-    file_path = Path(data_dir) / "pulled" / "fred_cpi.csv"
-    df = pd.read_csv(file_path, parse_dates=["DATE"])
-    df = df.set_index("DATE")
+    if from_cache:
+        file_path = Path(data_dir) / "pulled" / "fred_cpi.csv"
+        df = pd.read_csv(file_path, parse_dates=["DATE"])
+        df = df.set_index("DATE")
+    else:
+        # Load CPI, nominal GDP, and real GDP data from FRED, seasonally adjusted
+        df = pandas_datareader.get_data_fred(
+            ["CPIAUCNS", "GDP", "GDPC1"], start=start, end=end
+        )
+        if save_cache:
+            file_dir = Path(data_dir) / "pulled"
+            file_dir.mkdir(parents=True, exist_ok=True)
+            df.to_csv(file_dir / "fred_cpi.csv")
+            # df.to_parquet(file_dir / 'fred_cpi.parquet')
+
     # df.info()
     # df = pd.read_parquet(file_path)
     return df
 
 
 if __name__ == "__main__":
-    pull_and_save_fred_data(start="1913-01-01", end="2023-10-01", data_dir=DATA_DIR)
+    # Pull and save cache of fred data
+    load_fred(start="1913-01-01", end="2023-10-01", data_dir=DATA_DIR, from_cache=False)
