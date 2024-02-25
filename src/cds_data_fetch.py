c2b3ba3b0b5bf9ab3d5cd53cdf570204b5691aa1
Kausthub Keshava <64702003+kausthub-keshava@users.noreply.github.com>
2024-02-25 17:01:08 -0600

    adding_files_to_fetch_process_cds

---
+++ src/cds_data_fetch.py
@@ -0,0 +1,27 @@
+import config
+from pathlib import Path
+OUTPUT_DIR = Path(config.OUTPUT_DIR)
+DATA_DIR = Path(config.DATA_DIR)
+WRDS_USERNAME = config.WRDS_USERNAME
+import pickle
+import wrds
+import pandas as pd
+
+db = wrds.Connection(wrds_username=WRDS_USERNAME)
+
+def get_cds_data():
+    cds_data = {} 
+    for year in range(2001, 2024):  # Loop from 2001 to 2023
+        table_name = f"markit.CDS{year}"  # Generate table name dynamically
+        query = f"""
+        SELECT
+            date, ticker, parspread
+        FROM 
+            {table_name} a 
+        WHERE
+            a.tenor = '5Y' AND
+            a.country = 'United States'
+        """
+        cds_data[year] = db.raw_sql(query, date_cols=['date'])
+    return cds_data
+
