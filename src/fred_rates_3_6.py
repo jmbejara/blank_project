78d766769d08f70fcb69c90955e74c895245dc8c
Antonio Pineda <antoniopineda@uchicago.edu>
2024-02-25 17:45:32 -0600

    This will get the 3 and 6 months rates from the FRED. The web page I used was:
    https://fred.stlouisfed.org/series/DGS3MO and
    https://fred.stlouisfed.org/series/DGS6MO

---
+++ src/fred_rates_3_6.py
@@ -0,0 +1,36 @@
+import pandas_datareader.data as web
+import pandas as pd
+import numpy as np
+
+##########################################################
+
+series_descriptions = {
+    'DGS3MO': '3-Month Treasury Constant Maturity Rate',
+    'DGS6MO': '6-Month Treasury Constant Maturity Rate',
+}
+
+def pull_fred_data(start_date, end_date, ffill=True):
+    """
+    Pull data for the specified series from FRED.
+    """
+
+    # Using the same structure from HW1
+    df = web.DataReader(list(series_descriptions.keys()), 'fred', start_date, end_date)
+
+    if ffill:
+        for series in series_descriptions.keys():
+            df[series].ffill(inplace=True)
+
+    return df
+
+# Call the function to get the data
+start_date = '2001-01-02' # first value that is not NaN
+end_date = '2024-02-15'
+fred_data = pull_fred_data(start_date, end_date)
+
+
+fred_data #all the data are 6033 rows and 2 columns
+
+#store the data in a csv file
+#fred_data.to_csv('data\manual\fred_data.csv')
+
