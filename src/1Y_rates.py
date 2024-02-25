8f72f3873aac8e2fa8c7ea69d89ced4fa53c42a4
DianaCastellanos16 <146675278+DianaCastellanos16@users.noreply.github.com>
2024-02-25 17:01:41 -0600

    Add file of 1Y rates data

---
+++ src/1Y_rates.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd
+
+def fetch_yield_curve_data(url):
+    # Fetch the webpage content
+    response = requests.get(url)
+    if response.status_code == 200:
+        # Use pandas to read tables from the webpage content
+        dfs = pd.read_html(response.content)
+        
+        # Check if there are at least two tables
+        if len(dfs) >= 2:
+            # Adjusted to select the second table (index 1)
+            yield_curve_df = dfs[1]
+        else:
+            # Handle the case where there are fewer than two tables
+            yield_curve_df = dfs[0]
+        
+        # You can process the dataframe further as needed
+        return yield_curve_df
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+        return None
+
+# Base URL without the last number
+base_url = "https://www.federalreserve.gov/data/yield-curve-tables/feds200628_"
+
+
+dataframes = []
+
+for i in range(1, 14):
+    url = f"{base_url}{i}.html"
+    yield_curve_data = fetch_yield_curve_data(url)
+    
+    if yield_curve_data is not None:
+        
+        dataframes.append(yield_curve_data)
+
+all_data = pd.concat(dataframes, ignore_index=True)
+all_data.set_index('Date', inplace=True)
+
+all_data
